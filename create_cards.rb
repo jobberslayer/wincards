3642a907fc21fbb6cd580ce8ca178b11a78faf8a
Kevin Lester <klester@appriss.com>
2011-11-11 16:13:41 -0500

    cleanup

--- create_cards.rb
+++ create_cards.rb
@@ -155,36 +155,3 @@ card()
 print_sheet()
 
 puts "!!!DONE!!!"
-exit
-# Draw ellipse
-gc.stroke('red')
-gc.stroke_width(3)
-gc.fill_opacity(0)
-gc.ellipse(120, 150, 80, 120, 0, 270)
-
-# Draw endpoints
-gc.stroke('gray50')
-gc.stroke_width(1)
-gc.circle(120, 150, 124, 150)
-gc.circle(200, 150, 204, 150)
-gc.circle(120,  30, 124,  30)
-
-# Draw lines
-gc.line(120, 150, 200, 150)
-gc.line(120, 150, 120,  30)
-
-# Annotate
-gc.stroke('transparent')
-gc.fill('blue')
-gc.text(130, 35, "End")
-gc.text(188, 135, "Start")
-gc.text(130, 95, "'Height=120'")
-gc.text(55, 155, "'Width=80'")
-
-gc.text(5.in, 11.3.in, "BOTTOM") 
-gc.line(0.in, 11.4.in, 8.5.in, 11.4.in)
-
-gc.draw(canvas)
-canvas.write('shapes.gif')
-
-puts "!!!DONE!!!"
