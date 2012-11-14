6ed8764caad3cb1b1ac9e294a8af25018f38ad73
Kevin Lester <kevin@e-kevin>
2012-11-14 16:14:47 -0500

    created module for use with website

---
+++ WarLord.pm
@@ -0,0 +1,53 @@
+package WarLord;
+
+use warnings;
+use strict;
+use LWP::Simple;
+use URI::Escape;
+
+sub createPDF {
+    my ($data, $outfile) = @_;    
+
+    my $card_cache = '/home/sites/warlord/bin/wincards/cards';
+    my $out_dir = '/home/sites/warlord/bin/wincards';
+
+    my @img_files = ();
+
+    my $width = 390;
+    my $height = 546;
+
+    my $ct = 0;
+
+    my $files = "";
+    foreach my $rec (@$data) {
+        my ($num, $name) = ($$rec{num}, $$rec{name});
+    
+        if (!-e "/$name.jpg") {
+            get_card_image($name);    
+        }
+    
+        for (my $i = 0; $i < $num; $i++) {
+            $files .= "\"cards/$name.jpg\" ";
+        }
+    
+        $ct++;
+    }
+
+    system("montage -tile 3x3 -geometry ${width}x${height}+25+15 $files $outfile.pdf");
+}
+
+sub get_card_image {
+    my ($name) = @_;
+    
+    print "Caching $name\n";
+    
+    my $encoded_name = uri_escape($name);
+    my $url = "http://www.temple-of-lore.com/spoiler/popup.php?name=$encoded_name";
+    my $html = get($url);
+    $html =~ /IMG src="(.*?)"/;
+    print $1 . "\n";
+    my $img_url = "http://www.temple-of-lore.com/spoiler/$1";
+    getstore($img_url, "cards/$name.jpg");
+}
+
+1;
